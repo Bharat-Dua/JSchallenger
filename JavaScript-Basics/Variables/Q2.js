c034530b5131159b8a3a089f8f0551477bfba8ce
Bharat Dua <bharatdua13@gmail.com>
2024-07-24 23:14:48 +0530

    variables:solved- Assign a value to a variable

---
+++ JavaScript-Basics/Variables/Q2.js
@@ -0,0 +1,7 @@
+//* Assign a value to a variable *//
+
+//? Q:- Here, we declare the variable num. But, it has no value yet. Assign a value to it and run the code.
+
+let num;
+num = 2;
+console.log(num);

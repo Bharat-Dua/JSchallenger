97acea8f5be58041c7d115806905d84b056c8122
Bharat Dua <bharatdua13@gmail.com>
2024-07-28 21:59:25 +0530

    Booleans: solved- Assign boolean to a variable

---
+++ JavaScript-Basics/Booleans/Q1.js
@@ -0,0 +1,6 @@
+//* Assign boolean to a variable *//
+//? Here, we declare the variable isTrue. But, it has no value yet. Assign a boolean value to it and run the code.
+
+let isTrue;
+isTrue = true;
+console.log(isTrue);

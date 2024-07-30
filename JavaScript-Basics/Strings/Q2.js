0875a5558c77a35005b6d1640f3790d679780824
Bharat Dua <bharatdua13@gmail.com>
2024-07-30 07:06:46 +0530

    Strings:solved - Different ways to create Javascript strings

---
+++ JavaScript-Basics/Strings/Q2.js
@@ -0,0 +1,8 @@
+//* Different ways to create Javascript strings *//
+
+//? Here, we have declared 3 variables textOne, textTwo, and textThree. An empty string is assigned to all of them.Do you see how in each case different symbols are used to create the string? All three of them are valid methods to create a JavaScript string.Fill in all 3 strings with some characters and run the code to see if the values get logged.
+
+const textOne = "Hello, ";
+const textTwo = "it's ";
+const textThree = `me`;
+console.log(textOne, textTwo, textThree);

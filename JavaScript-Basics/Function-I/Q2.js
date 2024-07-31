17d233553add0b3b5066421cac38df4ce5c07770
Bharat Dua <bharatdua13@gmail.com>
2024-07-31 08:15:08 +0530

    Funciton-I:solved - Function expression

---
+++ JavaScript-Basics/Function-I/Q2.js
@@ -0,0 +1,8 @@
+//* Function expression *//
+//? In the code below, we introduced a small mistake when calling the function func. Find the mistake and run the code to see if the words hello world are correctly logged.
+
+const func = function () {
+  return "hello world";
+};
+const result = func();
+console.log(result);

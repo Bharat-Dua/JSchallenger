50a85ac8cc06c9abc1d813a53191699e9131da63
Bharat Dua <bharatdua13@gmail.com>
2024-08-02 21:48:58 +0530

    Async-JS:solved - Execute the code synchronously

---
+++ JavaScript-Basics/Async-JS/Q1.js
@@ -0,0 +1,10 @@
+//* Execute the code synchronously *//
+//? Adjust the code snippet so that the console.log statement logs the value 2.
+
+let i = 0;
+function func() {
+  i = 2;
+}
+func();
+// expected output = 2
+console.log(i);

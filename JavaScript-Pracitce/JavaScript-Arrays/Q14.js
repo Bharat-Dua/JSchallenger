be2ba1d0598dd097c49e0f1d92df3be9c5d05ac9
Bharat Dua <bharatdua13@gmail.com>
2024-08-13 16:07:13 +0530

    JS-Arrays:solved - Sort array by object property

---
+++ JavaScript-Pracitce/JavaScript-Arrays/Q14.js
@@ -0,0 +1,8 @@
+//* Sort array by object property *//
+
+//? Write a function that takes an array of objects as argument. Sort the array by property b in ascending order. Return the sorted array
+
+function myFunction(arr) {
+  const sort = (x, y) => x.b - y.b;
+  return arr.sort(sort);
+}
